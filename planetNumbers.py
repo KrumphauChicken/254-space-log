807b2bbd08c9fb1b785178e7036cdd2663da4bb8
andrewrivada <andrewrivada@csu.fullerton>
2018-04-04 20:16:12 -0700

    Added new file

---
+++ planetNumbers.py
@@ -0,0 +1,13 @@
+import re
+
+def findPlanets(content):
+
+
+    pattern = re.compile(r'Terraformable')
+    results = pattern.findall(content)
+    planetCounter = 0
+
+    for result in results:
+        planetCounter+=1
+
+    return planetCounter
