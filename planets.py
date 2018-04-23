465a74bc15e947d03d02a300474ca2d49231c794
Brian Frick <bfrick@fullerton.edu>
2018-04-22 19:23:05 -0700

    BUGFIX: get_planet_names was getting ALL stellar bodies

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 import re
 
 def get_planet_names(content:str):
-	pattern = re.compile("\"event\":\"Scan\", \"BodyName\":\"(.*?)\"")
+	pattern = re.compile("\"event\":\"Scan\", \"BodyName\":\"(.*?)\".*?(?=PlanetClass)")
 	result = pattern.findall(content)
 	return result
 
